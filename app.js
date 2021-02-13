0889d0423f2b13701b0e98409fb2cb45f897a88d
Arafin Ahmed <arafin2021@gmail.com>
2021-02-14 01:39:40 +0600

    my api key added

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let sliders = [];
 // If this key doesn't work
 // Find the name in the url and go to their website
 // to create your own api key
-const KEY = '15674931-a9d714b6e9d654524df198e00&q';
+const KEY = '4159513-810e14ce5b91a528cc52e3347&q';
 
 // show images 
 const showImages = (images) => {
@@ -117,8 +117,8 @@ const changeSlide = (index) => {
 }
 
 searchBtn.addEventListener('click', function () {
-  imageCounter();
   performSearch();
+  imageCounter();
 })
 
 sliderBtn.addEventListener('click', function () {
